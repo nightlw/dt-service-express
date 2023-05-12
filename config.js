45855b0de6b03c5980320fe6cd60b9d834245e11
13815893357 <renyong848@163.com>
2023-05-13 00:47:38 +0800

    ceshi

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MYSQL_DATANAME:'nodejs_demo',               // 数据库名称
+  MYSQL_USERNAME:'root',          // 数据库用户名
+  MYSQL_PASSWORD:'jBMf7QmA',        // 数据库密码
+  MYSQL_ADDRESS:'10.32.110.189:3306',        // 数据库地址数据库端口
+
+}
--- db.js
+++ db.js
@@ -1,32 +1,33 @@
-const { Sequelize, DataTypes } = require("sequelize");
+  const { Sequelize, DataTypes } = require("sequelize");
+  const config = require('./config')
 
-// 从环境变量中读取数据库配置
-const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
+  // 从环境变量中读取数据库配置
+  const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS, MYSQL_DATANAME= "" } = config;
 
-const [host, port] = MYSQL_ADDRESS.split(":");
+  const [host, port] = MYSQL_ADDRESS.split(":");
 
-const sequelize = new Sequelize("nodejs_demo", MYSQL_USERNAME, MYSQL_PASSWORD, {
-  host,
-  port,
-  dialect: "mysql" /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
-});
+  const sequelize = new Sequelize(MYSQL_DATANAME, MYSQL_USERNAME, MYSQL_PASSWORD, {
+    host,
+    port,
+    dialect: "mysql" /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
+  });
 
-// 定义数据模型
-const Counter = sequelize.define("Counter", {
-  count: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 1,
-  },
-});
+  // 定义数据模型
+  const Counter = sequelize.define("Counter", {
+    count: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+  });
 
-// 数据库初始化方法
-async function init() {
-  await Counter.sync({ alter: true });
-}
+  // 数据库初始化方法
+  async function init() {
+    await Counter.sync({ alter: true });
+  }
 
-// 导出初始化方法和模型
-module.exports = {
-  init,
-  Counter,
-};
+  // 导出初始化方法和模型
+  module.exports = {
+    init,
+    Counter,
+  };
